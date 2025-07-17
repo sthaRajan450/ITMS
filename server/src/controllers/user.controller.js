dcdfaf931288d4d450678cc244ef4d37a62a82df
sthaRajan450 <stharajan477@gmail.com>
2025-07-17 17:02:01 +0545

    Add SameSite attribute to cookies in loginUser for cross-site request handling

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -110,6 +110,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
+    samesite: "None", // Adjust based on your deployment
   };
 
   return res
