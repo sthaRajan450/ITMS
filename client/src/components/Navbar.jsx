8d17666e502afc87f51352d21740354b491504cc
sthaRajan450 <stharajan477@gmail.com>
2025-07-19 15:38:22 +0545

    style: Update Logout button to have rounded-full shape and white text

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -89,7 +89,7 @@ const Navbar = () => {
             <>
               <button
                 onClick={logoutUser}
-                className="bg-red-600  px-4 py-2 rounded-lg hover:bg-red-700 transition"
+                className="bg-red-600  px-4 py-2 rounded-full text-white hover:bg-red-700 transition"
               >
                 Logout
               </button>
